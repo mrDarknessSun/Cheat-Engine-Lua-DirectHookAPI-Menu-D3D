52e2ae7a61922eeb677ab942322906d135640566
mrDarknessSun <127573144+mrDarknessSun@users.noreply.github.com>
2023-03-13 00:08:40 -0300

    Add files via upload

---
+++ d3dBindPosition.lua
@@ -0,0 +1,21 @@
+
+
+
+
+_XD3D_Text_MenuHackOptionX = 22
+_XD3D_TextMenuHackOptionY = 48
+_XD3D_TextMenuHackOptionText = "Hack Menu :"
+_XD3D_TextDirectxInjectOption1String = "GOD : OFF"
+_XD3D_TextDirectxInjectOption2String = "Ak47 : OFF"
+_XD3D_TextDirectxInjectOption3String = "JUMP : OFF"
+
+_DirectXFontOption1_RenderStringOption1PosX = 30
+_DirectXFontOption1_RenderStringOption1PosY = _XD3D_TextMenuHackOptionY + _DirectXFontOption1_RenderStringOption1PosX +2
+
+
+_DirectXFontOption2_RenderStringOption2PosX = 30
+_DirectXFontOption2_RenderStringOption2PosY = _XD3D_TextMenuHackOptionY + _DirectXFontOption1_RenderStringOption1PosX +21
+
+
+_DirectXFontOption3_RenderStringOption3PosX = 30
+_DirectXFontOption3_RenderStringOption3PosY = _XD3D_TextMenuHackOptionY + _DirectXFontOption1_RenderStringOption1PosX +40
---
+++ dwBindColor.h
@@ -0,0 +1,6 @@
+
+
+setColorOptions_MenuBackground = clWhite
+setColorOptions_FunctionsKeyColor = clYellow
+d3d_setColor1=0xFF00000
+d3d_setColor2=0x0000FF
